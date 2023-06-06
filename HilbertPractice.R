d14a1431a2d527db04939586dec807a2a8860e3c
kaylaweldon <124632032+kaylaweldon@users.noreply.github.com>
2023-06-06 13:05:06 -0600

    practicing graphics

---
+++ HilbertPractice.R
@@ -0,0 +1,9 @@
+library(HilbertCurve)
+library(IRanges)
+
+# hc = HilbertCurve(1, 16, level = 2, reference = TRUE, title = "np = 3")
+# hc_points(hc, x1 = 1, x2 = 2, np = 3)
+
+
+hc = HilbertCurve(1, 100, level = 4, reference = TRUE)
+hc_points(hc, ir)
