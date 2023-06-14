0d837a174a55218942811ddc59e7daf4287b505b
kaylaweldon <124632032+kaylaweldon@users.noreply.github.com>
2023-06-14 15:59:45 -0600

    making a tree from a list

--- PythonTrees/Main.py
+++ PythonTrees/Main.py
@@ -1,9 +1,38 @@
 from Node import Node
 from Visualize import Visualize
+
 import matplotlib.pyplot as plt
+
+def constructTree(lst):
+    if not lst: 
+        return None
+
+    rootVal = lst[0]
+    root = Node(rootVal)
+
+    for item in lst[1:]:
+       # checks if item in list is a list
+        if isinstance(item, list):
+            child = constructTree(item)
+            root.add_child(child)
+        else:
+            child = Node(item)
+            root.add_child(child)
+            
+    return root
+
+mylist = ['A', ['B', 'E', 'F', ['H', 'I']], ['C'], 'D']
+rootNode = constructTree(mylist)
+
 class Main:
 
-    # Create nodes
+    # Visualize the tree
+    plt.figure(figsize=(6, 6))
+    visualization = Visualize.visualizeTree(rootNode)
+    plt.axis('off')
+    plt.show()
+
+    """ # Create nodes
     root = Node(1)
     child1 = Node(2)
     child2 = Node(3)
@@ -20,10 +49,4 @@ class Main:
     root.add_child(child4)
     child1.add_child(child5)
     child1.add_child(child6)
-    child6.add_child(child7)
-
-    # Visualize the tree
-    plt.figure(figsize=(6, 6))
-    Visualize.visualizeTree(root)
-    plt.axis('off')
-    plt.show()
+    child6.add_child(child7)"""
