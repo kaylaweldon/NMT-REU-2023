4b6ea6ea523a90218f24d3ff3e4d0d8ea972acbf
kaylaweldon <124632032+kaylaweldon@users.noreply.github.com>
2023-06-19 14:24:11 -0600

    testing new library

--- PythonTrees/NewTreeLib.py
+++ PythonTrees/NewTreeLib.py
@@ -1,27 +1,10 @@
 from Node import Node
 from Visualize import Visualize
-from ete3 import Tree
-
-def constructTree(lst):
-    if not lst: 
-        return None
-
-    rootVal = lst[0]
-    root = Node(rootVal)
-
-    for item in lst[1:]:
-       # checks if item in list is a list
-        if isinstance(item, list):
-            child = constructTree(item)
-            root.add_child(child)
-        else:
-            child = Node(item)
-            root.add_child(child)
-            
-    return root
+from ete3 import Tree, TreeStyle
 
 #t = Tree('A', ('B', 'E', 'F', ('H', 'I')), 'C', 'D')
-t = Tree( "((a,b),c);" )
 
-t.render("tree.png", w=183, units="mm")
+
+t = Tree( "((a,b),c);" )
+t.render("mytree.png", w=183, units="mm")
 
