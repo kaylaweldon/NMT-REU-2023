3ef92343aba9509cdf4e79bfa885dc7bba649494
kaylaweldon <124632032+kaylaweldon@users.noreply.github.com>
2023-06-19 10:59:12 -0600

    testing new library

--- PythonTrees/TestingTreeLists.py
+++ PythonTrees/TestingTreeLists.py
@@ -35,25 +35,6 @@ class Main:
     plt.axis('off')
     plt.show()
 
-    """ # Create nodes
-    root = Node(1)
-    child1 = Node(2)
-    child2 = Node(3)
-    child3 = Node(4)
-    child4 = Node(5)
-    child5 = Node(6)
-    child6 = Node(7)
-    child7 = Node(8)
-
-    # Link nodes together
-    root.add_child(child1)
-    root.add_child(child2)
-    root.add_child(child3)
-    root.add_child(child4)
-    child1.add_child(child5)
-    child1.add_child(child6)
-    child6.add_child(child7)"""
-
     ### TESTS OF RANDOM TREES
 
     # create new random tree generator
