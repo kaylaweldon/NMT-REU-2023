4aedfadcb20d021d51e59047cbb46d4066fdd43c
kaylaweldon <124632032+kaylaweldon@users.noreply.github.com>
2023-07-16 13:44:55 -0600

    more visualizing

--- PythonTrees/newListTree.py
+++ PythonTrees/newListTree.py
@@ -2,37 +2,17 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 class newListTree:
     from Forest import Forest
-    '''def numNodes(tree):
-        nodes = 0
-        for char in tree:
-            if char == '(':
-                nodes = nodes + 1
-        return nodes
-
-    def immediateChildren(tree):
-        children = 0
-        count = 0
-        for char in tree[2:]:
-            if char == '(':
-                count = count + 1
-            elif char == ')':
-                count = count - 1
-            if count == 0:
-                children = children + 1
-        return children
-    numVertices = numNodes(tree)
-    immediateChildren = immediateChildren(tree)'''
-
+    from DataToList import DataToList
     def __init__(self, trees):
         self.trees = trees
         self.fig = make_subplots(rows=1, cols=len(trees), shared_yaxes=True, horizontal_spacing=0.05)
 
 
     def buildTree(self, tree):
-        node = {'children': []}
+        node = {'children':[]}
         child_str = ''
         level = 0
-        for char in tree:
+        for char in tree[1:]:
             if char == '[':
                 level += 1
             elif char == ']':
@@ -112,53 +92,25 @@ class newListTree:
         Forest = self.Forest()
         Forest.generate_forest(4, 4, 4)
         niceForest = Forest.__get_forest__()
-        print(niceForest)
         lub = Forest.leastUpperBound(niceForest[0], niceForest[1])
-        print(lub)
         niceForest0 = newListTree.listToString(self, niceForest[0])
         niceForest1 = newListTree.listToString(self, niceForest[1])
+        print("Random Tree 1:")
         print(niceForest[0])
+        print("Random Tree 2:")
         print(niceForest[1])
+        print("Common Tree & LUB:")
+        print(lub)
         lubStr = newListTree.listToString(self, lub[0])
         treeList= [niceForest0, niceForest1, lubStr]
         tree_visualizer = newListTree(treeList)
         tree_visualizer.visualize_trees()
 
-tree1 = '[travel services, [travel agents]], [rv parks], [bed & breakfast], [hotels], [airports]'
-tree2 = '[a[b][c]]'
+        DataToList = self.DataToList()
+        treeLst = DataToList.dataToList()
+        treeLst = newListTree.listToString(self, treeLst)
+        tree_visualizer = newListTree(treeLst)
+        tree_visualizer.visualize_trees()
+
 if __name__ == '__main__':
-    newListTree([tree1, tree2]).main()
-
-[['x', ['x', ['j']], ['x', ['x', ['j'], ['Z'], ['K']]], [['p', ['T', ['w']]], 2], ['Q', ['D']], ['C', ['0']]], 9]
-['7', ['6', ['z']], ['3', ['9', ['j'], ['u'], ['U']]]]
-['5', ['g', ['j']], ['p', ['T', ['w']]], ['b', ['9', ['Z'], ['K']]], ['Q', ['D']], ['C', ['0']]]
-
-
-'''tree3 = ' ( ) '
-tree4 = '(((()()))(()())(()))'
-tree5 = ' ( )( ) '
-trees = [tree1, tree2, tree1, tree2]
-
-
-''treeDict1 = buildTree(tree1)
-    treeDict2 = buildTree(tree2)
-    assign_coordinates(treeDict1)
-    assign_coordinates(treeDict2)
-
-    fig = go.Figure()
-
-    if treeDict1 is not None:
-        add_trace(treeDict1)
-
-    if treeDict2 is not None:
-        add_trace(treeDict2)
-
-    fig.update_layout(
-        showlegend=False,
-        xaxis=dict(visible=False),
-        yaxis=dict(visible=False),
-        hovermode='closest',
-        plot_bgcolor='rgb(248,248,248)'
-    )
-
-    fig.show()'''
+    newListTree([[]]).main()
