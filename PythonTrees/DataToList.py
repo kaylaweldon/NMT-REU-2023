ce28f78550e5c107c1b76a3595712a01ae284173
kaylaweldon <124632032+kaylaweldon@users.noreply.github.com>
2023-07-16 11:49:12 -0600

    new lists

--- PythonTrees/DataToList.py
+++ PythonTrees/DataToList.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from Node import Node
-from Visualize import Visualize
+from newListTree import Visualize
 
 df = pd.read_excel('SocorroPOI.xlsx')
 
@@ -23,14 +23,14 @@ for _, row in df.iterrows():
     # If the category is already in the list, check if the subcategory is not already present
     if found_category:
         # If not null and not already listed, add it to the found_category list
-        if pd.notna(subcategory1) and subcategory1 not in found_category:
+        if pd.notna(subcategory1) and [subcategory1] not in found_category:
+            subcategory_entry = [subcategory1]
             if pd.notna(subcategory2):
                 if pd.notna(subcategory3):
-                    found_category.append([subcategory1, [subcategory2, subcategory3]])
+                    subcategory_entry.append([subcategory2, [subcategory3]])
                 else:
-                    found_category.append([subcategory1, [subcategory2]])
-            else:
-                found_category.append(subcategory1)
+                    subcategory_entry.append([subcategory2])
+            found_category.append(subcategory_entry)
 
     else:
         # If the category is not already in the list, add it with subcategories
@@ -38,43 +38,16 @@ for _, row in df.iterrows():
         if pd.notna(subcategory1):
             if pd.notna(subcategory2):
                 if pd.notna(subcategory3):
-                    category_entry.append([subcategory1, [subcategory2, subcategory3]])
+                    category_entry.append([subcategory1, [subcategory2, [subcategory3]]])
                 else:
                     category_entry.append([subcategory1, [subcategory2]])
             else:
-                category_entry.append(subcategory1)
+                category_entry.append([subcategory1])
         categories.append(category_entry)
-print(categories)
-
-
-def constructTree(lst):
-    if not lst: 
-        return None
-
-    rootVal = lst[0]
-    root = Node(rootVal)
-
-    for item in lst[1:]:
-       # checks if item in list is a list
-        if isinstance(item, list):
-            child = constructTree(item)
-            root.add_child(child)
-        else:
-            child = Node(item)
-            root.add_child(child)
-            
-    return root
-
-
+trees = []
 for category_entry in categories:
     print(category_entry)
-    rootNode = constructTree(category_entry)
-     # Visualize the tree
-    plt.figure(figsize=(6, 6))
-    visualization = Visualize.visualizeTree(rootNode)
-    plt.axis('off')
-    plt.show()
-
-
-
+"""    trees.extend(category_entry)
+    tree_visualizer = Visualize(trees)
+    tree_visualizer.visualize_trees()"""
 
