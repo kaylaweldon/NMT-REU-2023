e64b80d426b8a709d9e9181b5afd485d79189c8f
kaylaweldon <124632032+kaylaweldon@users.noreply.github.com>
2023-07-17 15:32:09 -0600

    yay

--- PythonTrees/RandomTreeGeneratorBetter.py
+++ PythonTrees/RandomTreeGeneratorBetter.py
@@ -88,7 +88,7 @@ class RandomTreeGeneratorBetter:
 
     def main(self):
 
-        print(self.generate(10, 6, ['root']))
+        print(self.generate(10, 6))
 
 if __name__ == '__main__':
     RandomTreeGeneratorBetter().main()
