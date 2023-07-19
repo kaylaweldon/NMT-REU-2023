d0b343847aedb362224cb6e4c37d26edd66ab918
kaylaweldon <124632032+kaylaweldon@users.noreply.github.com>
2023-07-19 13:10:58 -0600

    testing socorro visuals

--- PythonTrees/VisualizeSocorroForest.py
+++ PythonTrees/VisualizeSocorroForest.py
@@ -3,11 +3,11 @@ from plotly.subplots import make_subplots
 import math
 
 class newListTree:
+    from Forest import Forest
     from DataToList import DataToList
     def __init__(self, trees):
         self.trees = trees
         self.fig = make_subplots(rows=5, cols=4, shared_yaxes=True, horizontal_spacing=0.05)
-        self.fig.print_grid()
 
 
     def buildTree(self, tree):
@@ -81,46 +81,88 @@ class newListTree:
         )
 
         self.fig.show()
+    def listToString(self, lst): 
+        result = ""
+        if isinstance(lst, list):
+            result += "["
+            for item in lst:
+                result += self.listToString(item) + ", "
+            result = result.rstrip(", ") + "]"
+        else:
+            result += str(lst)
+        return result
 
-    def main(self):
+    """    def main(self):
         DataToList = self.DataToList()
         treeLst = DataToList.dataToList()
         tree_visualizer = newListTree(treeLst)
-        tree_visualizer.visualize_trees()
+        tree_visualizer.visualize_trees()"""
+
+    def main(self):
+        DataToList = self.DataToList()
+        original = DataToList.dataToList()
+        Forest = self.Forest()
+        Forest.setForest(original)
+        print("original forest:")
+        print(original)
+
+        treeList = []
+        for i, tree in enumerate(original):
+            treeList.append(self.listToString(tree))
+        
+        anonymousForest = Forest.anonymize_forest_greedy() 
+        print("anonymous forest greedy: ")
+        anonymousList = []
+        for i, tree in enumerate(anonymousForest[0]):
+            anonymousList.append(self.listToString(tree))
+        print(anonymousList)
+        print("pairs:")
+        print(anonymousForest[2])
+        print("Edit distance for anonymous Forest greedy:")
+        print(anonymousForest[1])
+
+        original = newListTree(treeList)
+        original.visualize_trees()
+        anonymized = newListTree(anonymousList)
+        anonymized.visualize_trees()
 
 if __name__ == '__main__':
-    tree_data = [
-        ['hotels & travel', ['travel services', ['travel agents']], ['rv parks'], ['bed & breakfast'], 
-         ['hotels'], ['airports']],
-        ['religious organizations', ['churches']],
-        ['automotive', ['gas stations'], ['truck rental', ['trailer rental']], ['body shop'],
-         ['auto repair'], ['car dealers'], ['auto detailing'], ['auto parts']],
-        ['arts & entertainment', ['rodeo']],
-        ['health & medical', ['hospitals'], ['home health care'], ['physical therapy'], ['dentists'],
-         ['counseling & mental health']],
-        ['food', ['food trucks'], ['coffee & tea'], ['bakeries'], ['grocery'], ['farmers market']],
-        ['nightlife', ['bars'], ['lounges']],
-        ['public services & government', ['landmarks & historical buildings'], ['parks'], ['museums'], 
-         ['police departments'], ['post offices']],       
-        ['local services', ['cemeteries'], ['community service/non-profit'], ['shipping centers'], 
-         ['forestry'], ['pest control'], ['funeral services & cemeteries'], ['community services/non-profit'],
-         ['appraisal services']],
-        ['restaurants', ['american (new)'], ['burgers'], ['breakfast & brunch', ['burgers', ['hot dogs']]],
-         ['chinese', ['buffets']], ['fast food', ['burgers', ['coffee & tea']]], ['fast food', 
-            ['burgers', ['ice cream & frozen yogurt']]], ['mexican'], ['pizza', ['fast food']], 
-            ['pizza', ['chicken wings', ['sandwiches']]], ['sandwiches', ['fast food']]],
-        ['pets', ['animal shelters'], ['veterinarians']],
-        ['shopping', ['discount stores'], ['mobile phones'], ['flowers & gifts'], ['fashion', ["women's clothing"]],
-         ['thrift stores'], ['home & garden', ['hardware stores']], ['home & garden'], ['art galleries'], 
-         ['flea markets'], ['department stores']],
-        ['education', ['middle & high schools'], ['elementary schools'], ['colleges & universities']],
-        ['active life', ['swimming pools'], ['fitness & instructions', ['gyms']], ['golf']],
-        ['financial services', ['banks & credit unions'], ['tax services'], ['insurance'], ['title loans']],
-        ['mass media ', ['radio stations'], ['print media']],
-        ['professional services', ['advertising'], ['web design'], ['business consulting'], ['accountants'], ['bookkeepers'], ['lawyers']],
-        ['home services', ['plumbing'], ['utilities', ['electricity suppliers']], ['real estate', ['real estate agents']], ['real estate ', ['property management']], ['real estate', ['home developers']], ['real estate', ['solar installation']]],
-        ['beauty & spas']]
-    tree_visualizer = newListTree(tree_data)
-    tree_visualizer.main()
-    """tree_visualizer = newListTree(None)
-    tree_visualizer.main()"""
+        newListTree([[]]).main() 
+        """niceForest = [
+    ['hotels & travel', ['travel services', ['travel agents']], ['rv parks'], ['bed & breakfast'], 
+        ['hotels'], ['airports']],
+    ['religious organizations', ['churches']],
+    ['automotive', ['gas stations'], ['truck rental', ['trailer rental']], ['body shop'],
+        ['auto repair'], ['car dealers'], ['auto detailing'], ['auto parts']],
+    ['arts & entertainment', ['rodeo']],
+    ['health & medical', ['hospitals'], ['home health care'], ['physical therapy'], ['dentists'],
+        ['counseling & mental health']],
+    ['food', ['food trucks'], ['coffee & tea'], ['bakeries'], ['grocery'], ['farmers market']],
+    ['nightlife', ['bars'], ['lounges']],
+    ['public services & government', ['landmarks & historical buildings'], ['parks'], ['museums'], 
+        ['police departments'], ['post offices']],       
+    ['local services', ['cemeteries'], ['community service/non-profit'], ['shipping centers'], 
+        ['forestry'], ['pest control'], ['funeral services & cemeteries'], ['community services/non-profit'],
+        ['appraisal services']],
+    ['restaurants', ['american (new)'], ['burgers'], ['breakfast & brunch', ['burgers', ['hot dogs']]],
+        ['chinese', ['buffets']], ['fast food', ['burgers', ['coffee & tea']]], ['fast food', 
+        ['burgers', ['ice cream & frozen yogurt']]], ['mexican'], ['pizza', ['fast food']], 
+        ['pizza', ['chicken wings', ['sandwiches']]], ['sandwiches', ['fast food']]],
+    ['pets', ['animal shelters'], ['veterinarians']],
+    ['shopping', ['discount stores'], ['mobile phones'], ['flowers & gifts'], ['fashion', ["women's clothing"]],
+        ['thrift stores'], ['home & garden', ['hardware stores']], ['home & garden'], ['art galleries'], 
+        ['flea markets'], ['department stores']],
+    ['education', ['middle & high schools'], ['elementary schools'], ['colleges & universities']],
+    ['active life', ['swimming pools'], ['fitness & instructions', ['gyms']], ['golf']],
+    ['financial services', ['banks & credit unions'], ['tax services'], ['insurance'], ['title loans']],
+    ['mass media ', ['radio stations'], ['print media']],
+    ['professional services', ['advertising'], ['web design'], ['business consulting'], ['accountants'], ['bookkeepers'], ['lawyers']],
+    ['home services', ['plumbing'], ['utilities', ['electricity suppliers']], ['real estate', ['real estate agents']], ['real estate ', ['property management']], ['real estate', ['home developers']], ['real estate', ['solar installation']]],
+    ['beauty & spas']]"""
+        """    greedy = Forest.anonymize_forest_greedy()
+    bruteForce = Forest.anonymize_forest_brute_force()""""""    greedyViz = newListTree(greedy)
+    greedyViz.main()
+
+    bfViz = newListTree(bruteForce)
+    bfViz.main()"""
+
